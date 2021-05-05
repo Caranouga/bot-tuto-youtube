80a054624725c5478d4f33b8815e961aa6cf63ae
Caranouga <62339171+Caranouga@users.noreply.github.com>
2021-05-05 18:20:34 +0200

    Ajout du bot sur github

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let prefix = "!!!";
+
+client.on('ready', () => {
+  console.log(`Connecté en tant que ${client.user.tag} !`)
+})
+
+client.on('message', msg => {
+  if(msg.content === `${prefix}ping`){
+    msg.reply('Pong !')
+  }
+});
+
+client.log('')
